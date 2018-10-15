d41cc669c3f3232533ac5267cfc5b064b3102c86
nicoleli11 <nicole.ll.930529@gmail.com>
2018-10-14 22:35:25 -0700

    addedindex.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //A
 function RandomRed(){
-
+	var red = Math.round(Math.random()*255)
+	return "rgb("+red+", 100, 100)";
 }
 
 
 setInterval(()=>{
 	app.style.backgroundColor = "rgb(255,255,255)";
-},500);
+}, 500);
