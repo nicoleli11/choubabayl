e6ed4c365e94b4b49074c6e9e77802989535b316
LinYang95 <bcityang@icloud.com>
2018-10-14 22:49:49 -0700

    fix the conflict

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ function RandomRed(){
 	return "rgb("+red+", 100, 100)";
 }
 
-
+ver i = 0;
 setInterval(()=>{
-	app.style.backgroundColor = "rgb(255,255,255)";
-<<<<<<< HEAD
+	if(i%2==0){
+	  app.style.backgroundColor = RandomRed();
+	} else {
+	app.styapp.style.backgroundColor = RandomBlue();
+	}
+	i++;
 },500);
 
 //B
@@ -15,6 +19,3 @@ function RandomBlue(){
         var blue = Math.round(Math.random()*255)
         return "rgb(100, 100, "+blue+")";
 }
-=======
-}, 500);
->>>>>>> d41cc669c3f3232533ac5267cfc5b064b3102c86
